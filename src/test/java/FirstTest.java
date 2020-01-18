5ec0409feaa2de97f8cfbb28f809736043fb16ae
sswietlik <sswietlik@gmail.com>
2020-01-18 09:43:35 +0100

    L1  Podpinanie: Selenium, JUnit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+    @Test
+    //@Test - to adnotacja z JUnit mówiąca że3 metoda pod nią jest testem
+    public void demoTest(){                         // METODA o nazwie demoTest
+        WebDriver driver = new ChromeDriver();      //Inicjacja WD tworzy chromedrivera - WD interfejs a CD klasa implementująca ten interface
+                        // Teraz podpinamy w POM "selenium" / zależności
+                        // Przechodzimy do POM a potem (Alt+Insert) lub ...
+                        // CODE -> GENERATE -> DEPENDENCY TEMPLATE -> I dodajemy z MVNRepository
+                        // Dorzucamy SeleniumJAVA, JUnit Jupiter,
+    }
+}
