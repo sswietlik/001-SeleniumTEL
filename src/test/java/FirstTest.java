8b550039a1c925c0381daead9b662c49a27c8134
sswietlik <sswietlik@gmail.com>
2020-01-18 16:31:29 +0100

    L1  Podpinanie: Selenium, JUnit - poprawione problemy z versja java

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -5,7 +5,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FirstTest {
     @Test
     //@Test - to adnotacja z JUnit mówiąca że3 metoda pod nią jest testem
-    public void demoTest(){                         // METODA o nazwie demoTest
+    public void demoTest(){
+        System.setProperty()
+
+
+        // METODA o nazwie demoTest
         WebDriver driver = new ChromeDriver();      //Inicjacja WD tworzy chromedrivera - WD interfejs a CD klasa implementująca ten interface
                         // Teraz podpinamy w POM "selenium" / zależności
                         // Przechodzimy do POM a potem (Alt+Insert) lub ...
